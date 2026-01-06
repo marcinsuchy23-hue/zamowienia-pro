94bc484bdee05a2135f833574640763058e3d632
marcinsuchy23-hue <marcin.suchy23@gmail.com>
2026-01-06 22:05:02 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump on every deploy: forces iOS/Safari to pick up the new UI
-const CACHE_NAME = 'zamowienia-pro-20260106m';
+const CACHE_NAME = 'zamowienia-pro-20260106n';
 const ASSETS = [
   "./",
   "./index.html",
